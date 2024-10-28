7d451fd99e1877662de568508ed43e9de7fd5f20
Fanlan <git@fanlan.net>
2024-10-28 17:23:44 +0800

    fix imgs path

--- Card.vue
+++ Card.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="container p-px mx-auto h-full">
-        <div :class="statusObj" class="border-t-8 w-4/5 bg-white mx-auto mt-4 border-gray-100 shadow p-5 rounded-md">
+        <div
+            :class="statusObj"
+            class="border-t-8 w-4/5 bg-white mx-auto mt-4 border-gray-100 shadow p-5 rounded-md"
+        >
             <div class="flex justify-center">
-                <img class="w-2/3 py-6" :src="imgStatus" alt="">
+                <img class="w-2/3 py-6" :src="imgStatus" alt="" />
             </div>
-            <h2 class="font-medium text-2xl mb-2">{{name}}</h2>
+            <h2 class="font-medium text-2xl mb-2">{{ name }}</h2>
             <div v-if="pDescription">
                 <div v-for="description in descriptions">
-                    <p>{{description}}</p>
+                    <p>{{ description }}</p>
                 </div>
             </div>
             <div v-else>
-                <p>{{status}}</p>
+                <p>{{ status }}</p>
             </div>
         </div>
     </div>
@@ -19,34 +22,32 @@
 
 <script>
 const statusColor = {
-    "無法辦理": "border-t-red-500",
-    "辦理完成": "border-t-green-500",
-    "提案排程中": "border-t-gray-500",
-    "辦理中": "border-t-blue-500",
-    "研議中": "border-t-yellow-500",
-    "提案中": "border-t-amber-500",
-}
+    無法辦理: "border-t-red-500",
+    辦理完成: "border-t-green-500",
+    提案排程中: "border-t-gray-500",
+    辦理中: "border-t-blue-500",
+    研議中: "border-t-yellow-500",
+    提案中: "border-t-amber-500",
+};
 
 export default {
-    name: 'card',
-    props: ['name','status','pDescription'],
+    name: "card",
+    props: ["name", "status", "pDescription"],
     data: () => {
-        return {
-
-        }
+        return {};
     },
     computed: {
         statusObj() {
             return {
                 [statusColor[this.status]]: true,
-            }
+            };
         },
         imgStatus() {
-            return `/imgs/${this.status}.png`
+            return `imgs/${this.status}.png`;
         },
         descriptions() {
-            return this.pDescription.split('\n')
-        }
-    }
-}
+            return this.pDescription.split("\n");
+        },
+    },
+};
 </script>
